4f170355630d6baa1c7069a80388094d0ba8998e
HasanMian <hasan_aslam@icloud.com>
2024-05-05 19:52:54 -0400

    Update word_guessing_game.py

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -8,3 +8,18 @@ colors = [ 'red', 'orange', 'black', 'gray', 'blue', 'green', 'purple', 'yellow'
 
 words = animals + fruits + colors
 random_guess = random.choice(words)
+print ("WORD GUESSING GAME")
+if random_guess in animals:
+  print('Hint: It is a mysterious Animal')
+elif random_guess in fruit: 
+  print('Hint: It is a mysterious Fruit')
+else: 
+    print('Hint: It is a mysterious Color')
+
+user_guesses = ''
+chances = 3
+
+while chances  > 0:
+    wrong_guess  = 0
+    for ch in random_guess:
+      
