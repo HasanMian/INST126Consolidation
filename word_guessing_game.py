daa190c90491a15a51f045a1196dd08801fdafe1
HasanMian <hasan_aslam@icloud.com>
2024-05-05 23:26:10 -0400

    Update word_guessing_game.py
    Completed the coding aspect of the project. I experienced a bunch of indentation errors with my code because the guess block and the if wrong_guess was inside the while loop.

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -11,7 +11,7 @@ random_guess = random.choice(words)
 print ("WORD GUESSING GAME")
 if random_guess in animals:
   print('Hint: It is a mysterious Animal')
-elif random_guess in fruit: 
+elif random_guess in fruits: 
   print('Hint: It is a mysterious Fruit')
 else: 
     print('Hint: It is a mysterious Color')
@@ -27,10 +27,16 @@ while chances  > 0:
         else: 
             print('*', end=" ") # placeholder for letters not guessed
             wrong_guess += 1
-        if wrong_guess == 0:
-            print('/nCongratulations you guessed correctly:', random_guess)
+    if wrong_guess == 0:
+            print('\nCongratulations you guessed correctly:', random_guess)
             break
-        guess  = input('\nGuess a letter or the entire word: ')
-        user_guesses += guess
+    guess  = input('\nGuess a letter or the entire word: ') # user can either enter a single character or guess the entire word
+    user_guesses += guess
+
+    if guess not in random_guess:
+        chances -= 1
+        print(f'Wrong! You have {chances} more chances')
+        if chances == 0:
+          print('Game Over! You Guessed inncorectly, You Lost. The correct word is: ', random_guess)
       
       
